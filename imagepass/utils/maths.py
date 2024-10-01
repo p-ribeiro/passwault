71b5f02f4345b57446400ddb10b2b0915dbbac10
Pedro Ribeiro <pedromr21@gmail.com>
2024-09-30 22:36:11 -0400

    feat: password generator

--- imagepass/utils/maths.py
+++
@@ -1,7 +0,0 @@
-from typing import List
-
-
-def int_to_bin(int_list: List[int]) -> List[str]:
-    bytes_list: List[bytes] = [bin(val) for val in int_list]
-    
-    return bytes_list
---
+++ imagepass/utils/utils.py
@@ -0,0 +1,36 @@
+from random import choice    
+from typing import List
+
+
+def password_generator(len: int, has_symbols: bool, has_digits: bool, has_uppercase: bool) -> str:
+    
+    SYMBOLS_RANGE = [33, 38]
+    DIGITS_RANGE = [48, 57]
+    UPPERCASE_RANGE = [65, 90]
+    LOWERCASE_RANGE = [97, 122]
+    
+    
+    pool = ([i for i in range(LOWERCASE_RANGE[0], LOWERCASE_RANGE[1] + 1)])
+    
+    if has_symbols:
+        pool.extend([i for i in range(SYMBOLS_RANGE[0], SYMBOLS_RANGE[1] + 1)])
+        
+    if has_digits:
+        pool.extend([i for i in range(DIGITS_RANGE[0], DIGITS_RANGE[1] + 1)])
+        
+    if has_uppercase:
+        pool.extend([i for i in range(UPPERCASE_RANGE[0], UPPERCASE_RANGE[1] + 1)])
+
+    
+    password = "".join([chr(choice(pool)) for _ in range(len)])
+    
+    return password
+    
+
+
+def int_to_bin(int_list: List[int]) -> List[str]:
+    bytes_list: List[bytes] = [bin(val) for val in int_list]
+    
+    return bytes_list
+ 
+    
