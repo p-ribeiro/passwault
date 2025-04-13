ce42d00dbabc4c85f557c92b2cb451df19bff426
Pedro <pedromr21@gmail.com>
2025-04-13 00:39:44 -0400

    test: 🧪  fixed tests

--- tests/test_authenticator.py
+++ tests/test_authenticator.py
@@ -7,22 +7,14 @@ fake_time = datetime(2024, 1, 1, 12, 0, 0, tzinfo=timezone.utc)
 
 @patch("src.passwault.core.commands.authenticator.get_password_with_mask", return_value="fake_password")
 @patch("src.passwault.core.commands.authenticator.Logger")
-@patch("src.passwault.core.commands.authenticator.UserRepository")
-def test_register_success(mock_user_repo_cls, mock_logger, mock_get_password):
-    mock_connector = MagicMock()
-    session_manager = MagicMock()
-    session_manager.connector = mock_connector
-
+def test_register_success(mock_logger, mock_get_password):
     mock_user_repo = MagicMock()
     mock_user_repo.check_if_username_exists.return_value.ok = True
     mock_user_repo.check_if_username_exists.return_value.result = False
     mock_user_repo.register.return_value.ok = True
 
-    mock_user_repo_cls.return_value = mock_user_repo
+    register("johndoe", None, "user", mock_user_repo)
 
-    register("johndoe", None, "user", session_manager)
-
-    mock_user_repo_cls.assert_called_once_with(mock_connector)
     mock_user_repo.check_if_username_exists.assert_called_once_with("johndoe")
     mock_user_repo.register.assert_called_once_with("johndoe", "fake_password", "user")
     mock_logger.info.assert_called_with("User created.")
@@ -30,22 +22,14 @@ def test_register_success(mock_user_repo_cls, mock_logger, mock_get_password):
 
 @patch("src.passwault.core.commands.authenticator.get_password_with_mask", return_value="fake_password")
 @patch("src.passwault.core.commands.authenticator.Logger")
-@patch("src.passwault.core.commands.authenticator.UserRepository")
-def test_register_username_taken(mock_user_repo_cls, mock_logger, mock_get_password):
-    mock_connector = MagicMock()
-    session_manager = MagicMock()
-    session_manager.connector = mock_connector
-
+def test_register_username_taken(mock_logger, mock_get_password):
     mock_user_repo = MagicMock()
     mock_user_repo.check_if_username_exists.return_value.ok = True
     mock_user_repo.check_if_username_exists.return_value.result = True
     mock_user_repo.register.return_value.ok = True
 
-    mock_user_repo_cls.return_value = mock_user_repo
+    register("johndoe", None, "user", mock_user_repo)
 
-    register("johndoe", None, "user", session_manager)
-
-    mock_user_repo_cls.assert_called_once_with(mock_connector)
     mock_user_repo.check_if_username_exists.assert_called_once_with("johndoe")
     mock_logger.info.assert_called_with("This username is already taken. Please provide another.")
     mock_user_repo.register.assert_not_called()
@@ -53,23 +37,15 @@ def test_register_username_taken(mock_user_repo_cls, mock_logger, mock_get_passw
 
 @patch("src.passwault.core.commands.authenticator.get_password_with_mask", return_value="fake_password")
 @patch("src.passwault.core.commands.authenticator.Logger")
-@patch("src.passwault.core.commands.authenticator.UserRepository")
-def test_register_fail(mock_user_repo_cls, mock_logger, mock_get_password):
-    mock_connector = MagicMock()
-    session_manager = MagicMock()
-    session_manager.connector = mock_connector
-
+def test_register_fail(mock_logger, mock_get_password):
     mock_user_repo = MagicMock()
     mock_user_repo.check_if_username_exists.return_value.ok = True
     mock_user_repo.check_if_username_exists.return_value.result = False
     mock_user_repo.register.return_value.ok = False
     mock_user_repo.register.return_value.result = "Error while registering"
 
-    mock_user_repo_cls.return_value = mock_user_repo
+    register("johndoe", None, "user", mock_user_repo)
 
-    register("johndoe", None, "user", session_manager)
-
-    mock_user_repo_cls.assert_called_once_with(mock_connector)
     mock_user_repo.check_if_username_exists.assert_called_once_with("johndoe")
     mock_user_repo.register.assert_called_once_with("johndoe", "fake_password", "user")
     mock_logger.error.assert_called_with("Error while registering")
@@ -94,11 +70,10 @@ def test_login_success(mock_user_repo_cls, mock_logger, mock_get_password, mock_
 
     mock_user_repo_cls.return_value = mock_user_repo
 
-    login("johndoe", None, mock_session_manager)
+    login("johndoe", None, mock_session_manager, mock_user_repo)
 
-    mock_user_repo_cls.assert_called_once_with(mock_connector)
     mock_user_repo.authentication.assert_called_once_with("johndoe", "fake_password")
-    mock_user_repo.get_role.assert_called_once_with("johndoe")
+    mock_user_repo.get_role.assert_called_once()
     mock_session_manager.create_session.assert_called_once_with({"id": 13, "role": 1, "time": fake_time.isoformat()})
     mock_logger.info.assert_called_once_with("User logged in")
 
@@ -121,9 +96,8 @@ def test_login_not_authenticated(mock_user_repo_cls, mock_logger, mock_get_passw
 
     mock_user_repo_cls.return_value = mock_user_repo
 
-    login("johndoe", None, mock_session_manager)
+    login("johndoe", None, mock_session_manager, mock_user_repo)
 
-    mock_user_repo_cls.assert_called_once_with(mock_connector)
     mock_user_repo.authentication.assert_called_once_with("johndoe", "fake_password")
     mock_user_repo.get_role.assert_not_called()
     mock_session_manager.create_session.assert_not_called()
@@ -149,10 +123,9 @@ def test_login_role_not_found(mock_user_repo_cls, mock_logger, mock_get_password
 
     mock_user_repo_cls.return_value = mock_user_repo
 
-    login("johndoe", None, mock_session_manager)
+    login("johndoe", None, mock_session_manager, mock_user_repo)
 
-    mock_user_repo_cls.assert_called_once_with(mock_connector)
     mock_user_repo.authentication.assert_called_once_with("johndoe", "fake_password")
-    mock_user_repo.get_role.assert_called_once_with("johndoe")
+    mock_user_repo.get_role.assert_called_once()
     mock_session_manager.create_session.assert_not_called()
     mock_logger.error.assert_called_once_with("Role not found")
